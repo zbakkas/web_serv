132740f9c3ab8c1d0a19d4dae075418a8555c45d
zbakkas <zouhirbakkas@gmail.com>
2025-01-27 23:42:27 +0100

    cgi2

--- test.php
+++ test.php
@@ -3,7 +3,8 @@ $query_string = getenv('QUERY_STRING');
 parse_str($query_string, $params);
 
 // Debugging output
-echo "<pre>" . print_r($params, true) . "</pre>";
+// echo "<pre>" . print_r($params, true) . "</pre>";
 
-echo "Hello, " . $params['name'] . "!";
+
+echo  "<p>Hello, " . $params['name'] . "!</p>";
 ?>
